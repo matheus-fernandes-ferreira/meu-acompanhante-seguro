50559cbdaccfd878cc0afbec6f6c4f8f8b7e2cb2
DevLucas <luksilvafe@gmail.com>
2024-06-10 17:13:45 -0300

    Atualização de segunda-feira 17:13

--- src/assets/js/acordeon.js
+++ src/assets/js/acordeon.js
@@ -3,7 +3,9 @@ const botoesDoAcordeoon2 = document.querySelector(".botao-protecao");
 const botoesDoAcordeoon3 = document.querySelector(".botao-contaminacao");
 const botoesDoAcordeoon4 = document.querySelector(".botao-leia-mais");
 const area = document.querySelector(".area-expandida");
-const area2 = document.querySelector(".area-expandida-protecao");
+const area2 = document.querySelector(".area-expandida-2");
+const area3 = document.querySelector(".area-expandida-3");
+const area4 = document.querySelector(".area-expandida-4");
 
 botoesDoAcordeoon1.addEventListener("click", () =>
     area.classList.toggle("active"));
@@ -12,8 +14,8 @@ botoesDoAcordeoon2.addEventListener("click", () =>
     area2.classList.toggle("active"));
 
 botoesDoAcordeoon3.addEventListener("click", () =>
-    area.classList.toggle("active"));
+    area3.classList.toggle("active"));
 
 botoesDoAcordeoon4.addEventListener("click", () =>
-    area.classList.toggle("active"));
+    area4.classList.toggle("active"));
 
