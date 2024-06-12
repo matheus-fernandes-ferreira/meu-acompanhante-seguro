8fa6fad3536d57f7c8558b363f4fc65eb1d1d4c6
DevLucas <luksilvafe@gmail.com>
2024-06-12 01:20:08 -0300

    Atualização de quarta 1:20am

--- src/assets/js/acordeon.js
+++ src/assets/js/acordeon.js
@@ -22,3 +22,19 @@ botoesDoAcordeoon4.addEventListener("click", () =>
 function test(){
     console.log("ola eduanea!");
 }
+
+
+var acc = document.getElementsByClassName("acordeon-mobile");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
